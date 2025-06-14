334a4c1751cc5cd13a4d24f8d41a3c5a829aec14
Rantetsu Inori <shuishenme@outlook.com>
2025-06-14 16:24:48 +0800

    refactor(eslint): add max props length per line

--- eslint.config.js
+++ eslint.config.js
@@ -361,6 +361,10 @@ export default [
 			"vue/no-dupe-keys": "off",
 			"vue/no-v-for-template-key": "off", // 官方说明：它会和 vue/no-v-for-template-key-on-child 规则打架。
 			"vue/v-on-event-hyphenation": ["error", "never", { "autofix": true }],
+			"vue/max-attributes-per-line": ["error", {
+				"singleline": 5,
+				"multiline": 1,
+			}],
 			"no-restricted-properties": ["error", {
 				object: "arguments",
 				property: "callee",
